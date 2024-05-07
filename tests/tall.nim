48d72aa36c85585873c83b3487c607d259368464
rockcavera <rockcavera@gmail.com>
2024-05-07 00:19:43 -0300

    added ch. 29 tests

--- tests/tall.nim
+++ tests/tall.nim
@@ -1,5 +1,6 @@
 import ./tcompile, ./tassignment, ./tblock, ./tbool, ./tcall, ./tclass,
        ./tclosure, ./tcomments, ./tconstructor, ./texpressions, ./tfield,
-       ./tfor, ./tfunction, ./tif, ./tlimit, ./tlogicaloperator, ./tmethod,
-       ./tnil, ./tnumber, ./toperator, ./tothers, ./tprint, ./tregression,
-       ./treturn, ./tstring, ./tthis, ./tvariable, ./twhile
+       ./tfor, ./tfunction, ./tif, ./tinheritance, ./tlimit, ./tlogicaloperator,
+       ./tmethod, ./tnil, ./tnumber, ./toperator, ./tothers, ./tprint,
+       ./tregression, ./treturn, ./tstring, ./tsuper, ./tthis, ./tvariable,
+       ./twhile
